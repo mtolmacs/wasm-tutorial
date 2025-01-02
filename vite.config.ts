63ce05d8384c5a436238778b05b7f16848d74a08
Mark Tolmacs <mark@lazycat.hu>
2025-01-02 14:57:18 +0100

    Automatically build and reload the WASM binary on change

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
+import wasmPackWatchPlugin from "vite-plugin-wasm-pack-watcher";
 
 export default defineConfig({
-  plugins: [wasm(), topLevelAwait()],
+  build: {
+    watch: {
+      include: ["src/**/*.ts", "src/**/*.rs"],
+    },
+  },
+  plugins: [wasmPackWatchPlugin(), wasm(), topLevelAwait()],
 });
